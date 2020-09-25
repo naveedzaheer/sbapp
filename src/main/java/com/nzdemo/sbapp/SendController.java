f9f07110fd46999025c30c20eacd32dede4401d1
Naveed Zaheer <nzaheer@microsoft.com>
2020-09-25 07:41:41 -0500

    Code Updates

--- src/main/java/com/nzdemo/sbapp/SendController.java
+++ src/main/java/com/nzdemo/sbapp/SendController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SendController {
 
-    private static final String DESTINATION_NAME = "greetings";
+    private static final String DESTINATION_NAME = "nzjms2q";
 
     private static final Logger logger = LoggerFactory.getLogger(SendController.class);
 
@@ -23,6 +24,13 @@ public class SendController {
     public Greeting postMessage(@RequestBody Greeting greeting) {
         logger.info("Sending message");
         jmsTemplate.convertAndSend(DESTINATION_NAME, greeting.toString());
+        return greeting;
+    }
+
+    @GetMapping("/messages")
+	public String getMessage() {
+        logger.info("Receiving message");
+        String greeting = (String)jmsTemplate.receiveAndConvert(DESTINATION_NAME);
         return greeting;
     }
 }
